97edd64633164e3af1f2616a9daa3977f0340217
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-16 19:22:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  
+if array.length == 1 
+  array.first
+elsif array.length == 2
+  array.join(" and ")
+  elsif array.length >= 3
+  x = array.pop 
+  # array = ['kiwi', 'durian']   x = 'starfruit'
+ array.join(", ") + ", " + "and " + x
+end
 end
